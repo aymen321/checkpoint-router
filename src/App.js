387c27ad8beab4a38fe9562b412929c8a66e2d6c
aymen321 <bouazziaymen7@gmail.com>
2022-03-12 20:22:25 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Movies from './Components/Movies/Movies';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*Movie component*/}
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
